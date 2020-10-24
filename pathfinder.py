31b7b61af9496f1907f12c830e19f61b457a57b9
Islambek <islambek.mamet@nu.edu.kz>
2020-10-25 00:19:02 +0600

    added pathfinding class

--- pathfinder.py
+++ pathfinder.py
@@ -1,49 +1,176 @@
 import pygame
 import sys
-
+from queue import PriorityQueue
+import math
 
 pygame.init()
 
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
 YELLOW = (255,255,0)
-BACKGROUND_COLOR = (0,0,0)
+BLACK = (0,0,0)
+WHITE = (255,255,255)
 GREY = (180, 180, 180)
+GREEN = (255, 0, 255)
+ORANGE = (255,140,0)
 
 finish = False
-width = 900
-rows = 30
+width = 800
+rows = 10
+columns = 10
 margin = width // rows
 
-screen = pygame.display.set_mode((width, width))
+screen = pygame.display.set_mode((800, 800))
 
-class drawGrid():
-    def __init__(self, screen, rows, color = YELLOW):
-        x = 0
-        y = 0
+class grid():
+    def __init__(self, pos,rows,columns, color = WHITE):
+        self.x, self.y = pos
         self.color = color
-        for l in range(rows):
-            x = x + margin
-            y = y + margin
-            pygame.draw.line(screen, (255,255,255), (x,0),(x,width))
-            pygame.draw.line(screen, (255,255,255), (0,y),(width,y))
+        self.neighbors = []
+        self.rows = rows
+        self.columns = columns
+        self.gCost = 0
+        self.hCost = 0
+        self.fCost = 0
+    def closeSet(self):
+        self.color = BLACK
+    def openSet(self):
+        self.color = YELLOW
+    def startPos(self):
+        self.color = GREEN
+    def endPos(self):
+        self.color = BLUE
+    def getPath(self):
+        self.color = RED
+    def getPos(self):
+        return [self.x, self.y]
+    def draw(self, screen, margin):
+        pygame.draw.rect(screen, self.color, (self.x * margin, self.y * margin, margin, margin))
+    def getNeighbors(self, grids):
+        self.neighbors = []
+        if self.x < self.rows - 1:
+            self.neighbors.append(grids[self.x + 1][self.y])    # NEIGHBOR AT RIGHT
+        if self.x > 0:
+            self.neighbors.append(grids[self.x - 1][self.y])    # NEIGHBOR AT LEFT
+        if self.y < self.columns - 1:
+            self.neighbors.append(grids[self.x][self.y + 1])    # NEIGHBOR AT BOTTOM
+        if self.y > 0:
+            self.neighbors.append(grids[self.x][self.y - 1])    # NEIGHBOR AT TOP
+    def neighborsPos(self):
+        pass
 
-    def draw(self, pos):
-        pos[0] = pos[0] // margin * margin
-        pos[1] = pos[1] // margin * margin
 
-        pygame.draw.rect(screen, self.color, (pos[0], pos[1], rows, rows))
 
-grid = drawGrid(screen, rows)
+class a_pathfinding:
+    def __init__(self, start, end, grids, draw):
+        self.openQ = PriorityQueue()
+        self.counter = 0
+        self.closedSet = set()
+        self.openQ.put((0, self.counter, start))
+        self.all_path = {start}
+        self.draw = draw
+        self.start = start
+        self.end = end
+        # self.gCosts = {each_grid: float("inf") for grid in grids for each_grid in grid}
+        # self.gCosts[start] = 0
+        self.fCosts = {each_grid: float("inf") for grid in grids for each_grid in grid}
+        self.fCosts[start] = self.distance_of_grids(self.start.getPos(), self.end.getPos())
 
+    def pathfinder(self):
+        while not self.openQ.empty():
+            current = self.openQ.get()[2]
+            currentIndex = 0
+            self.all_path.add(current)
+            self.closedSet.add(current)
+
+            if current == self.end:
+                while current in self.all_path:
+                    current.getPath()
+                    self.draw()
+
+            for neighbor in current.neighbors:
+                for closed_neighbor in self.closedSet:
+                    if neighbor == closed_neighbor:
+                        neighbor.closeSet()
+                        continue
+
+                neighbor.gCost = current.gCost + 1
+                neighbor.hCost = self.distance_of_grids(neighbor.getPos(), self.end.getPos())
+                neighbor.fCost = neighbor.gCost + neighbor.hCost
+                if neighbor.fCost < self.fCosts[neighbor]:
+                    self.all_path.add(neighbor)
+                    self.fCosts[neighbor] = neighbor.fCost
+                    # self.gCosts[neighbor] = neighbor.gCost
+                    self.counter += 1
+                    self.openQ.put((neighbor.fCost, self.counter, neighbor))
+                    neighbor.openSet()
+                    self.closedSet.add(neighbor)
+
+            self.draw()
+            if current != self.start:
+                current.closeSet()
+
+    def distance_of_grids(self, pos1, pos2):
+        x1, y1 = pos1
+        x2, y2 = pos2
+        return abs(x2 - x1) + abs(y2 - y1)
+
+def drawGrid(screen, margin, rows, columns):
+    for i in range(rows):
+        pygame.draw.line(screen, BLACK, (i * margin, 0), (i* margin, width))
+        for j in range(columns):
+            pygame.draw.line(screen, BLACK, (0, j * margin), (width, j * margin))
+
+def createGrids(columns, rows):
+    grids = []
+    for x in range(rows):
+        grids.append([])
+        for y in range(columns):
+            each_grid = grid([x, y], rows, columns)
+            grids[x].append(each_grid)
+    return grids
+
+def getPosByMouse(pos, margin):
+    pos[0] = pos[0] // margin
+    pos[1] = pos[1] // margin
+    return [pos[0], pos[1]]
+
+def draw_update(screen, grids, rows, columns, margin):
+
+    for grid in grids:
+        for each_grid in grid:
+            each_grid.draw(screen, margin)
+
+    drawGrid(screen, margin, rows, columns)
+    pygame.display.update()
+
+start = None
+end = None
+grids = createGrids(columns,rows)
+print(grids[1][2])
 while not finish:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            print(pygame.mouse.get_pos())
-            grid.draw(list(pygame.mouse.get_pos()))
+        if pygame.mouse.get_pressed()[0]:
+            x, y = getPosByMouse(list(pygame.mouse.get_pos()), margin)
+            new_grid = grids[x][y]
+            if not start and not end:
+                start = new_grid
+                start.startPos()
+            elif start and not end:
+                end = new_grid
+                end.endPos()
+                algorithm = a_pathfinding(start, end, grids, lambda : draw_update(screen, grids, rows, columns, margin))
+            elif start and end:
+                for grid in grids:
+                    for each_grid in grid:
+                        each_grid.getNeighbors(grids)
+                algorithm.pathfinder()
 
-
-    # screen.fill(BACKGROUND_COLOR)
-    pygame.display.update()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_c:
+                start = None
+                end = None
+                grid = make_grid(ROWS, width)
+    draw_update(screen, grids, rows, columns, margin)
--- test.py
+++ test.py
@@ -1,49 +1,158 @@
 import pygame
 import sys
-
+from queue import PriorityQueue
+import math
 
 pygame.init()
 
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
 YELLOW = (255,255,0)
-BACKGROUND_COLOR = (0,0,0)
+BLACK = (0,0,0)
+WHITE = (255,255,255)
 GREY = (180, 180, 180)
+GREEN = (255, 0, 255)
+ORANGE = (255,140,0)
 
 finish = False
-width = 900
-rows = 30
+width = 800
+rows = 10
+columns = 10
 margin = width // rows
 
-screen = pygame.display.set_mode((width, width))
+screen = pygame.display.set_mode((800, 800))
 
-class drawGrid():
-    def __init__(self, screen, rows, color = YELLOW):
-        x = 0
-        y = 0
+class grid():
+    def __init__(self, pos,rows,columns, color = WHITE):
+        self.x, self.y = pos
         self.color = color
-        for l in range(rows):
-            x = x + margin
-            y = y + margin
-            pygame.draw.line(screen, (255,255,255), (x,0),(x,width))
-            pygame.draw.line(screen, (255,255,255), (0,y),(width,y))
+        self.neighbors = []
+        self.rows = rows
+        self.columns = columns
+        self.gCost = 0
+        self.hCost = 0
+        self.fCost = 0
+    def closeSet(self):
+        self.color = BLACK
+    def openSet(self):
+        self.color = YELLOW
+    def startPos(self):
+        self.color = GREEN
+    def endPos(self):
+        self.color = BLUE
+    def getPath(self):
+        self.color = RED
+    def getPos(self):
+        return [self.x, self.y]
+    def draw(self, screen, margin):
+        pygame.draw.rect(screen, self.color, (self.x * margin, self.y * margin, margin, margin))
+    def getNeighbors(self, grids):
+        self.neighbors = []
+        if self.x < self.rows - 1:
+            self.neighbors.append(grids[self.x + 1][self.y])    # NEIGHBOR AT RIGHT
+        if self.x > 0:
+            self.neighbors.append(grids[self.x - 1][self.y])    # NEIGHBOR AT LEFT
+        if self.y < self.columns - 1:
+            self.neighbors.append(grids[self.x][self.y + 1])    # NEIGHBOR AT BOTTOM
+        if self.y > 0:
+            self.neighbors.append(grids[self.x][self.y - 1])    # NEIGHBOR AT TOP
+        return self.neighbors
+    def neighborsPos(self):
 
-    def draw(self, pos):
-        pos[0] = pos[0] // margin * margin
-        pos[1] = pos[1] // margin * margin
+def distance_of_grids(pos1, pos2):
+    return math.sqrt((pos2[0] - pos1[0])**2 + (pos2[1] - pos1[1])**2)
 
-        pygame.draw.rect(screen, self.color, (pos[0], pos[1], rows, rows))
+def a_pathfinding(start, end, grids):
+    openSet = []
+    closedSet = []
+    start.fCost = start.gCost = start.hCost = 0
+    end.fCost = end.gCost = end.hCost = 0
+    openList.append(start)
+    count = 0
+    path = set()
+    path.add(start)
 
-grid = drawGrid(screen, rows)
+    while len(openList) > 0:
+        current = openList[count]
+        currentIndex = count
 
+        for index, item in enumerate(openList):
+            if item.fCost < current.fCost:
+                current = itemvnm
+                currentIndex = index
+
+        openList.pop(currentIndex)
+        closedList.append(current)
+
+        if current == end:
+            path
+            while current is not None:
+                path.append(current.getPos())
+                current = current.parent
+
+            return path[::-1]
+
+        for neighbor in current.getNeighbors(grids):
+            for closed_neighbor in closedList:
+                if neighbor == closed_neighbor:
+                    continue
+
+            neighbor.gCost = current.gCost + 1
+            neighbor.hCost = distance_of_grids(neighbor.)
+            neighbor.fCost = neighbor.gCost + neighbor.hCost
+
+            for openGrids in openList:
+                if neighbor == openGrids and neighbor.gCost > openGrids.gCost:
+                    continue
+            openList.append(neighbor)
+
+
+
+def drawGrid(screen, margin, rows, columns):
+    for i in range(rows):
+        pygame.draw.line(screen, BLACK, (i * margin, 0), (i* margin, width))
+        for j in range(columns):
+            pygame.draw.line(screen, BLACK, (0, j * margin), (width, j * margin))
+
+def createGrids(columns, rows):
+    grids = []
+    for x in range(rows):
+        grids.append([])
+        for y in range(columns):
+            each_grid = grid([x, y], rows, columns)
+            grids[x].append(each_grid)
+    return grids
+
+def getPosByMouse(pos, margin):
+    pos[0] = pos[0] // margin
+    pos[1] = pos[1] // margin
+    return [pos[0], pos[1]]
+
+def draw_update(screen, grids, rows, columns, margin):
+
+    for grid in grids:
+        for each_grid in grid:
+            each_grid.draw(screen, margin)
+
+    drawGrid(screen, margin, rows, columns)
+    pygame.display.update()
+
+start = None
+end = None
+grids = createGrids(columns,rows)
 while not finish:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            print(pygame.mouse.get_pos())
-            grid.draw(list(pygame.mouse.get_pos()))
-
-
-    # screen.fill(BACKGROUND_COLOR)
-    pygame.display.update()
+        if pygame.mouse.get_pressed()[0]:
+            x, y = getPosByMouse(list(pygame.mouse.get_pos()), margin)
+            new_grid = grids[x][y]
+            if not start and not end:
+                start = new_grid
+                start.startPos()
+            elif start and not end:
+                end = new_grid
+                end.endPos()
+            elif start and end:
+                pass
+    draw_update(screen, grids, rows, columns, margin)
